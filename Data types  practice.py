9bb8c73edb8068ffd940d3985663a2f61021c7d2
Iqra Arif <bintearif79@gmail.com>
2025-05-06 06:37:20 -0700

    Add files via upload

---
+++ Data types  practice.py
@@ -0,0 +1,17 @@
+# integer variable: Whole numbers without any decimal point.
+# Examle 
+age=20
+marks=85
+print("Age is:",age)
+print("Marks is:",marks)
+#String variable: The text, words or a characters is a string ,anything write in quotes.
+# Example
+name="iqra"
+subject="computer"
+print("Name is:",name)
+print("Subject is:",subject)
+#Float variable:Numbers that have decimal points
+speed=45.75
+volume=1.25
+print("Speed is=",speed)
+print("Volume is=",volume)
